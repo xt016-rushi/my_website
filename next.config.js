6f95c72dbed2f7a36cc40439ef5e4d021486753d
Rushi <rushi123@gmail.com>
2025-10-30 18:54:38 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const { MIDDLEWARE_FILENAME } = require('next/dist/lib/constants');
+const { Chakra_Petch, Volkhov } = require('next/font/google');
+const { jsx } = require('react/jsx-runtime');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
 module.exports = nextConfig;
+
+  
